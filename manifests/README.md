6954e730659800c9333821b41dfa3aba0a4e2b9f
Martin Westergaard Lassen <mwl@trifork.com>
2014-07-07 11:39:27 +0100

    Initial module structure

---
+++ manifests/README.md
@@ -0,0 +1,18 @@
+Riak Puppet module
+===
+
+Example usage
+---
+
+```
+class {"riak":
+    version => 1.4.9-1,
+    node_name => "riak@%{::ipaddress_eth0}",
+    ring_creation_size => 64,
+    http_address => "127.0.0.1,
+    backend_default => "eleveldb"
+    backends => [],
+    vmargs_pa => "/usr/lib/relai",
+    vmargs_s = "riak_sync"
+}
+```
---
+++ manifests/init.pp
@@ -0,0 +1,52 @@
+class riak(
+    $version = "1.4.9-1",
+    $node_name = "riak@127.0.0.1",
+    $ring_creation_size = 64,
+    $http_address = "127.0.0.1",
+    $backend_default => "bitcask"
+    $backends = [],
+    $vmargs_pa = "",
+    $vmargs_s = "") {
+
+    $version_split = split($version, ".")
+    $version_minor = "${version_split[0]}.${version_split[1]}"
+    $version_patch = "${version_split[0]}.${version_split[1]}.${version_split[2]}"
+    $package_name = "riak-${version}_amd64.deb"
+    $package_location = "/tmp/${riak_package_name}"
+    $download_url = "http://s3.amazonaws.com/downloads.basho.com/riak/${version_minor}/${version_patch}/ubuntu/precise/${package_name}" #TODO: detect Ubuntu version
+
+    exec {"download-riak":
+        command => "wget -o ${package_location} ${download_url}",
+        unless => "echo ```curl -s ${download_url}.sha``` \\*${package_location} | sha1sum -c -",
+    }
+
+	package {"libssl0.9.8":
+	    ensure => installed,
+	}
+
+    package {"riak":
+        provider => "dpkg",
+        ensure => latest,
+        source => $package_location,
+        require => [Exec["download-riak"], Package["libssl0.9.8"]]
+    }
+
+    file {"/etc/riak/vm.args":
+        ensure => file,
+        mode => 644,
+        require => Package["riak"],
+        content => template("riak/vm.args")
+    }
+
+    file {"/etc/riak/app.config":
+        ensure => file,
+        mode => 644,
+        require => Package["riak"],
+        content => template("riak/app.config")
+    }
+
+    service {"riak":
+        ensure => running,
+        require => [File["/etc/riak/vm.args"], File["/etc/riak/app.config"]]
+    }
+}
